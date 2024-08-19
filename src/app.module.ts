5954da1ff7bf50540e19b2491f014b67e7d44224
husainf4l <husain.f4l@gmail.com>
2024-08-19 22:06:32 +0300

    update cart

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { PrismaService } from './prisma/prisma.service';
-import { CartModule } from './cart/cart.module';
-import { OrderModule } from './order/order.module';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'husain',
-      password: 'tt55oo77',
-      database: 'toppikdb',
-      entities: [ProductEntity],
-      synchronize: true,
-      logging: true,
-    }),
     ProductModule,
   ],
   controllers: [AppController],
